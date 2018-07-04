37d10493a86e24b990f5e57872e324e2aa5521df
umeshbharti <40784402+umeshbharti@users.noreply.github.com>
2018-07-04 14:32:23 +0530

    Add files via upload

---
+++ src/main/java/springBootTest/HomeController.java
@@ -0,0 +1,12 @@
+package springBootTest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello!!";
+	}
+}
---
+++ src/main/java/springBootTest/SpringBootWebApplication.java
@@ -0,0 +1,11 @@
+package springBootTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;  
+@SpringBootApplication  
+public class SpringBootWebApplication {  
+    public static void main(String[] args) {  
+        SpringApplication.run(SpringBootWebApplication.class, args);  
+        System.out.println("Hello Spring boot");
+    }  
+}  	
