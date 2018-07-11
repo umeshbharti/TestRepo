a462c5c4e35958aaf90f34d4cdbc3917b476a32e
Umesh Bharti <umesh.bharti@capgemini.com>
2018-07-11 15:01:19 +0530

    This is test

---
+++ src/main/java/springBootTest/Test.java
@@ -0,0 +1,53 @@
+package springBootTest;
+
+
+
+class A{
+
+	{
+		System.out.println("A's instanse block");
+	}
+	static{
+		System.out.println("A's static block");
+	}
+	
+	A(){
+		System.out.println("A's constructor");
+	}
+}
+
+class B extends A{
+
+	{
+		System.out.println("B's instanse block");
+	}
+	static{
+		System.out.println("B's static block");
+	}
+	
+	B(){
+		System.out.println("B's constructor");
+	}
+}
+
+public class Test extends B{
+
+	{
+		System.out.println("CallTest's instanse block 1111");
+	}
+	static{
+		System.out.println("CallTest's static block");
+	}
+	
+	Test(){
+		System.out.println("CallTest's constructor");
+	}
+	public static void main(String[] args) {
+		
+		System.out.println("Before");
+		new Test();
+		System.out.println("After");
+	}
+
+}
+
